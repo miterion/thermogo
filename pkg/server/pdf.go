5ac0d6433feb6e47ef4411b90bd71a398d3d494c
Heiko Carrasco <heiko.carrasco@yahoo.com>
2019-05-21 18:31:05 +0200

    Fix encoding problem for utf8 parameters

--- pkg/server/pdf.go
+++ pkg/server/pdf.go
@@ -1,14 +1,21 @@
 package server
 
 import (
-	"strconv"
 	"bytes"
-	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
+	"fmt"
 	"io"
 	"log"
 	"os/exec"
+	"strconv"
 	"strings"
+
+	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
 )
+
+func embedhead(html string) string {
+	head := "<head><meta charset='UTF-8'> </head><body>%s</body>"
+	return fmt.Sprintf(head, html)
+}
 
 func GeneratePDF(html string, w io.Writer) {
 	pdfg, err := wkhtmltopdf.NewPDFGenerator()
@@ -19,7 +26,7 @@ func GeneratePDF(html string, w io.Writer) {
 	pdfg.Grayscale.Set(true)
 	pdfg.PageWidth.Set(72)
 	pdfg.PageHeight.Set(100)
-	pdfg.AddPage(wkhtmltopdf.NewPageReader(strings.NewReader(html)))
+	pdfg.AddPage(wkhtmltopdf.NewPageReader(strings.NewReader(embedhead(html))))
 
 	pdfg.SetOutput(w)
 
