13698e6d2c1eab99e91f022bd28ae832ef504b44
Heiko Carrasco <heiko.carrasco@yahoo.com>
2020-08-24 10:57:58 +0200

    Moved main to more correct place

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,11 @@
-package main
+package cmd
 
 import (
 	"flag"
 	"github.com/miterion/thermogo/pkg/server"
 )
 
-func main() {
+func Execute() {
 	var port = flag.Int("port", 3000, "Port number for the server")
 	flag.Parse()
 	server.Run(*port)
